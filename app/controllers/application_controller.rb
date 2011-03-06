58eb71ab99d2a45ad00ea36d7a1ddee6ed4bdb3b
Jonathan Hall <jonathan.edward.hall@gmail.com>
2011-03-05 21:27:20 -0500

    Users controller gets CanCan authorization, Appication Controller handles CanCan Access Denied now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization
+  
+  rescue_from CanCan::AccessDenied do |exception|  
+    flash[:error] = "Access denied!"  
+    redirect_to root_url  
+  end
 end
